98f1e2647bd1b96d8713bf1ce203e675424c36e1
Nutifafa Attor <attorfafa@gmail.com>
2022-03-08 16:48:30 +0000

    header and nav

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 /*=============== SHOW MENU ===============*/
-
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
