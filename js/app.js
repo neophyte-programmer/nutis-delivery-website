099021a070ce7fa6d38ee49f3d65282897e38976
Nutifafa Attor <attorfafa@gmail.com>
2022-03-13 13:59:44 +0000

    scroll up functionality

--- js/app.js
+++ js/app.js
@@ -53,6 +53,11 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/
