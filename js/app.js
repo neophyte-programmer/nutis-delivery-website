24fedef463a4610c639ce99f5039bb1edb57ad76
Nutifafa Attor <attorfafa@gmail.com>
2022-03-14 09:52:37 +0000

    dark theme functionality

--- js/app.js
+++ js/app.js
@@ -61,31 +61,31 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/
-// const themeButton = document.getElementById('theme-button')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'bx-sun'
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
 
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
 
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
-// }
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
 
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
